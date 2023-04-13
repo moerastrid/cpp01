e30d8d6073b5cbf451b0836f2120fa4e16b8bcdf
astrid <moerastrid@gmail.com>
2023-04-13 14:39:42 +0200

    betere versie cpp01 ex04

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,13 +6,13 @@
 /*   By: ageels <ageels@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/31 15:03:30 by ageels        #+#    #+#                 */
-/*   Updated: 2023/04/13 14:27:02 by ageels        ########   odam.nl         */
+/*   Updated: 2023/04/13 14:37:53 by ageels        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "inc/head.h"
 
-void	set_text(std::string &text, std::fstream &file)
+void	get_text(std::string &text, std::fstream &file)
 {
 	std::string		line;
 
@@ -24,6 +24,21 @@ void	set_text(std::string &text, std::fstream &file)
 		getline(file, line);
 	}
 	text.append(line);
+}
+
+void	set_text(std::string &text, char **argv)
+{
+	std::size_t		found(0);
+
+	found = text.find(argv[2]);
+	while (found != std::string::npos)
+	{
+		text.erase(found, ((std::string)argv[2]).length());
+		text.insert(found, argv[3]);
+		found += ((std::string)argv[3]).length();
+		
+		found = text.find(argv[2], found);
+	}
 }
 
 bool	ok_args(int argc, char **argv, std::fstream &file)
@@ -47,25 +62,16 @@ int main(int argc, char **argv)
 	std::fstream	f_source;
 	std::fstream	f_dest;
 	std::string		text;
-	std::size_t		found(0);
 	std::string		newname;
 	
 	if (ok_args(argc, argv, f_source) == false)
 		return (1);
 	newname = argv[1];
 	newname.insert(newname.length(), ".replace");
-	set_text(text, f_source);
+	get_text(text, f_source);
 	f_source.close();
+	set_text(text, argv);
 	f_dest.open(newname, std::fstream::in | std::fstream::out | std::fstream::trunc);
-	found = text.find(argv[2]);
-	while (found != std::string::npos)
-	{
-		text.erase(found, ((std::string)argv[2]).length());
-		text.insert(found, argv[3]);
-		found += ((std::string)argv[3]).length();
-		
-		found = text.find(argv[2], found);
-	}
 	f_dest << text;
 	f_dest.close();
 	return (0);
